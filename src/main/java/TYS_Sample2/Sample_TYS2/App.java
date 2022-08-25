81b4f1e91109230b97e446638efa848af5fc515e
admin <admin@DESKTOP-SSRV3EC>
2022-08-25 11:47:53 +0530

    Sample_Created

---
+++ src/main/java/TYS_Sample2/Sample_TYS2/App.java
@@ -0,0 +1,13 @@
+package TYS_Sample2.Sample_TYS2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/TYS_Sample2/Sample_TYS2/sample_2.java
@@ -0,0 +1,7 @@
+package TYS_Sample2.Sample_TYS2;
+
+public class sample_2 {
+public static void main(String[] args) {
+	System.out.println("hello lead");
+}
+}
